ff2607fe8decfd7a9af771845eb614c642f77720
J0eyhacksgibsons <69731355+J0eyhacksgibsons@users.noreply.github.com>
2022-01-11 02:25:37 -0800

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,5 @@
-import caesar_art
-print(caesar_art.logo)
+from caesar_art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def casesar(start_text,shift_amount,cipher_direction):
     final_text = ""
